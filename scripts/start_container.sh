5fdd0976ae73c1903776dc2693dfd80f6a8d7721
Shra1Yadav <93590332+Shra1Yadav@users.noreply.github.com>
2024-08-30 00:27:10 +0530

    add files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull shra1995yadav/python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 shra1995yadav/python-flask-app
